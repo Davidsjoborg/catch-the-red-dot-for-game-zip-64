331db06fb255aa392f718036f1cde3083364641d
Davidsjoborg <154664943+Davidsjoborg@users.noreply.github.com>
2026-05-22 17:14:26 +0200

    Forgot display show

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ GAME_ZIP64.onButtonPress(GAME_ZIP64.ZIP64ButtonPins.Up, GAME_ZIP64.ZIP64ButtonEv
     display2 = GAME_ZIP64.createZIP64Display()
     display.clear()
     display.setMatrixColor(5, 5, GAME_ZIP64.colors(ZipLedColors.Blue))
+    display.show()
 })
 let display2: GAME_ZIP64.ZIP64Display = null
 let display: GAME_ZIP64.ZIP64Display = null
